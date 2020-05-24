7c858f712e8765495cfad5953796fc6fa957aa92
Jun Aruga <jaruga@redhat.com>
2020-05-24 18:48:00 +0200

    Add comments.

--- home_learning/download-sequences/retrive_sequence.sh
+++ home_learning/download-sequences/retrive_sequence.sh
@@ -10,17 +10,24 @@ fi
 echo "${#ACCESSIONS[@]}"
 echo "${ACCESSIONS[@]}"
 mkdir -p out
+# Use EFetch API to download the files.
+# https://dataguide.nlm.nih.gov/eutilities/utilities.html#efetch
+# https://www.ncbi.nlm.nih.gov/books/NBK25499/table/chapter4.T._valid_values_of__retmode_and/?report=objectonly
 for ACCESSION in ${ACCESSIONS[@]}; do
-    # Whole nucleotide sequence of the genome: .fna (rettype: fasta)
+    # Whole nucleotide sequence of the genome: .fna
+    # FASTA (rettype: fasta, retmode: text)
     wget -O "out/$ACCESSION.fna" "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=$ACCESSION&rettype=fasta&retmode=text"
     sleep 1
-    # Fasta sequence of coresponding nucleotide: .ffn (rettype: fasta_cds_na)
+    # Fasta sequence of coresponding nucleotide: .ffn
+    # CDS nucleotide FASTA (rettype: fasta_cds_na, retmode: text)
     wget -O "out/$ACCESSION.ffn" "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=$ACCESSION&rettype=fasta_cds_na&retmode=text"
     sleep 1
-    # Fasta sequence of amino acid: .faa (rettype: fasta_cds_aa)
+    # Fasta sequence of amino acid: .faa
+    # CDS protein FASTA (rettype: fasta_cds_aa, retmode: text)
     wget -O "out/$ACCESSION.faa" "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=$ACCESSION&rettype=fasta_cds_aa&retmode=text"
     sleep 1
-    # GenBank record: .gbk (rettype: gbwithparts)
+    # GenBank record: .gbk
+    # GenBank flat file with full sequence (contigs) (rettype: gbwithparts, retmode: text)
     wget -O "out/$ACCESSION.gbk" "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=$ACCESSION&rettype=gbwithparts&retmode=text"
     sleep 1
 done
